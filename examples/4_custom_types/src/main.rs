9c598abadcbda4011525b781b90f2f8a1204d978
Daniel Serezane <serezane@protonmail.com>
2022-10-17 12:02:24 -0300

    mais exemplos

--- examples/4_custom_types/src/main.rs
+++ examples/4_custom_types/src/main.rs
@@ -78,7 +78,7 @@ fn main() {
 // names and type information together specify the variant:
 // `PageLoad != PageUnload` and `KeyPress(char) != Paste(String)`.
 // Each is different and independent.
-enum WebEvent {
+/*enum WebEvent {
     // An `enum` may either be `unit-like`,
     PageLoad,
     PageUnload,
@@ -118,4 +118,21 @@ fn main() {
     inspect(click);
     inspect(load);
     inspect(unload);
+}*/
+
+type NanoSecond = u64;
+type Inch = u64;
+type U64 = u64;
+
+fn main() {
+    // `NanoSecond` = `Inch` = `U64` = `u64`.
+    let nanoseconds: NanoSecond = 5 as U64;
+    let inches: Inch = 2 as U64;
+
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+             nanoseconds,
+             inches,
+             nanoseconds + inches);
 }
---
+++ examples/6_2_conv_avancada/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // Convertendo o primitivo str pro tipo não primitivo String
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+    /*
+    Sim, o Rust tem um tipo chamado String com letra maiúscula.
+    Quem programa POO deve estar se perguntando: pô, tem um tipo que é um wrapper
+    de um tipo primitivo, e esse tipo wrapper tem métodos, funções, acopladas a ele,
+    inclusive de conversão... saporra é uma classe!
+    Calma, não é classe. Tem cara de classe, tem cheiro de classe, mas não é classe.
+     */
+}
+
+// Podemos criar conversores para nossos tipos customizados usando a trait From
+// Por hora, pense na trait como "uma funçãozinha do tipo". Quem sabe POO e principalmente
+// quem já programou C++ já sacou como funciona ou se não sacou vai sacar agora.
+// Enfim, depois a gente explica melhor, por enquanto é só pra mostrar.
+// NÃO PRECISA COPIAR.
+/*use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+    // Com a trait From definida para o nosso tipo, 
+    //podemos nos aproveitar da inferência da linguagem usando a trait Into
+    let int = 5;
+    // Try removing the type declaration
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}*/
