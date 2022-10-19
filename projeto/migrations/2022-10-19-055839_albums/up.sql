9e0fbcdfc9fa0f9a7ee00ac2b81142e90b8315e5
Daniel Serezane <serezane@protonmail.com>
2022-10-19 08:48:28 -0300

    projeto

--- projeto/migrations/2022-10-19-055839_albums/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE albums (
-    id integer primary key autoincrement not null,
-    artist varchar(64) not null,
-    title varchar(64) not null,
-    cover_art_url varchar(256) not null,
-    label varchar(32) not null,
-    release_date date not null
-)
