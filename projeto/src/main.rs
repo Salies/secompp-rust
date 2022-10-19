13c14a936c7a1cd34ace777e564277da8126f61b
Daniel Serezane <serezane@protonmail.com>
2022-10-19 04:57:11 -0300

    exemplo: base completa

--- projeto/src/main.rs
+++ projeto/src/main.rs
@@ -1,11 +1,15 @@
+// Declarando nossos módulos
 mod schema;
 mod model;
-
+mod routes;
+// Módulos de Crates externas
 use crate::diesel::prelude::*;
 use rocket::{ response::status::{Created, NoContent, NotFound}, serde::json::Json };
+use rocket_sync_db_pools::{database, diesel};
+// Usando nossos módulos
 use model::{Album, NewAlbum};
 use schema::albums;
-use rocket_sync_db_pools::{database, diesel};
+use routes::*;
 
 #[database("sqlite_data")]
 struct DbConn(diesel::SqliteConnection);
@@ -18,6 +22,6 @@ fn rocket() -> _ {
         // Routes
         .mount(
             "/artists",
-            rocket::routes![list, retrieve, create, update, destroy],
+            rocket::routes![all, all_by_artist, get_by_id, delete]
         )
 }
